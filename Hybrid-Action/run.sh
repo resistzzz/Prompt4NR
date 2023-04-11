cecacedbf0c3f5ffd55216b586c95678327bc6c7
resistzzz <resistzzz97@gmail.com>
2023-04-11 21:01:23 +0800

    Source code of Hybrid-Action

--- Hybrid-Action/run.sh
+++ Hybrid-Action/run.sh
@@ -1,1 +1,15 @@
+python main-multigpu.py --data_path ../DATA/MIND-Small --train_data_path ../DATA/MIND-Small --epochs 5 --batch_size 16 --test_batch_size 100 --wd 1e-3 --max_tokens 500 --log True --model_save True
+python predict.py --data_path ../DATA/MIND-Small --ratio 0.5 --test_batch_size 100 --max_tokens 500 --model_file ./temp/BestModel.pt --log True
+
+python main-multigpu.py --data_path ../DATA/MIND-Small --train_data_path ../DATA/MIND-Small-0.5 --epochs 5 --batch_size 16 --test_batch_size 100 --wd 1e-3 --max_tokens 500 --log True --model_save True
+python predict.py --data_path ../DATA/MIND-Small --ratio 0.5 --test_batch_size 100 --max_tokens 500 --model_file ./temp/BestModel.pt --log True
+
+python main-multigpu.py --data_path ../DATA/MIND-Small --train_data_path ../DATA/MIND-Small-0.3 --epochs 5 --batch_size 16 --test_batch_size 100 --wd 1e-3 --max_tokens 500 --log True --model_save True
+python predict.py --data_path ../DATA/MIND-Small --ratio 0.3 --test_batch_size 100 --max_tokens 500 --model_file ./temp/BestModel.pt --log True
+
+python main-multigpu.py --data_path ../DATA/MIND-Small --train_data_path ../DATA/MIND-Small-0.2 --epochs 5 --batch_size 16 --test_batch_size 100 --wd 1e-3 --max_tokens 500 --log True --model_save True
+python predict.py --data_path ../DATA/MIND-Small --ratio 0.2 --test_batch_size 100 --max_tokens 500 --model_file ./temp/BestModel.pt --log True
+
+python main-multigpu.py --data_path ../DATA/MIND-Small --train_data_path ../DATA/MIND-Small-0.1 --epochs 5 --batch_size 16 --test_batch_size 100 --wd 1e-3 --max_tokens 500 --log True --model_save True
+python predict.py --data_path ../DATA/MIND-Small --ratio 0.1 --test_batch_size 100 --max_tokens 500 --model_file ./temp/BestModel.pt --log True
 
