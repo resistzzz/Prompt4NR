0b2e64a97a79f3fa91c371b707982bf5f20e8289
NicoleSa <nicolesang.nicochan@gmail.com>
2024-06-06 15:19:11 +0200

    Run python scripts unbuffered such that prints in the code will be immediately outputted.

--- Discrete-Action/run_py3.sh
+++ Discrete-Action/run_py3.sh
@@ -1,3 +1,3 @@
 # Compatible with python version 3
-python3 main-multigpu.py --data_path ../DATA/MIND-Small --epochs 3 --batch_size 16 --test_batch_size 100 --wd 1e-3 --max_tokens 500 --log True --model_save True
-python3 predict.py --data_path ../DATA/MIND-Small --test_batch_size 100 --max_tokens 500 --model_file ./temp/BestModel.pt --log True
+python3 -u main-multigpu.py --data_path ../DATA/MIND-Small --epochs 3 --batch_size 16 --test_batch_size 100 --wd 1e-3 --max_tokens 500 --log True --model_save True
+python3 -u predict.py --data_path ../DATA/MIND-Small --test_batch_size 100 --max_tokens 500 --model_file ./temp/BestModel.pt --log True
