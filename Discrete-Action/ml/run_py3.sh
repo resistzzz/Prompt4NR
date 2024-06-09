665c0dca2f43c7c44d3ae6eb174739f4bc3665b7
Nicole Sang-Ajang <scur1608@int4.local.snellius.surf.nl>
2024-06-09 19:34:11 +0200

    Removed redundant recsys_gpu.yml. Leading recsys_gpu.yml is in the rootfolder of the framework.

--- Discrete-Action/ml/run_py3.sh
+++
@@ -1,2 +0,0 @@
-python3 ../main-multigpu.py --data_path ../../DATA/MIND-Small --model_name bert-base-multilingual-uncased --epochs 3 --batch_size 16 --test_batch_size 100 --wd 1e-3 --max_tokens 500 --log True --model_save True
-python3 ../predict.py --data_path ../../DATA/MIND-Small --test_batch_size 100 --max_tokens 500 --model_file .././temp/BestModel.pt --log True
