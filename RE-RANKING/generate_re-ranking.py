316662331c2e2dc208ae6b9453c09273ae8e1d5d
Nicole Sang <nicolesang.nicochan@gmail.com>
2024-06-23 13:03:58 +0200

    Setup re-ranking with icf (inverse click frequency).

---
+++ RE-RANKING/generate_re-ranking.py
@@ -0,0 +1,6 @@
+if __name__ == "__main__":
+    # Read in dataset
+
+    # * Processing behaviors
+    # By default non existent features in test set are: Article ID, Next Readtime, Next Scroll Percentage, and Clicked Article IDs.
+    # Dropppinng where article_id == empty
