e536cb6e806dc58eeea8db9e7f99df2eb3dade42
NicoleSa <nicolesang.nicochan@gmail.com>
2024-06-28 10:53:58 +0200

    Cleaned up history selection code

--- Discrete-Action/_run.sh
+++
@@ -1,3 +0,0 @@
-# Unused file
-python main-multigpu.py --data_path ../DATA/MIND-Small --epochs 3 --batch_size 16 --test_batch_size 100 --wd 1e-3 --max_tokens 500 --log True --model_save True
-python predict.py --data_path ../DATA/MIND-Small --test_batch_size 100 --max_tokens 500 --model_file ./temp/BestModel.pt --log True
